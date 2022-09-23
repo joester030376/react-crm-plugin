af791eca96450f278481494f31038bb13efbc581
joester030376 <firehawkdigitalohio@gmail.com>
2022-09-22 23:37:01 -0400

    Added Intake Form framework. Still needs validation and submission code to the repository.

--- src/components/Forms/IntakeForm.js
+++ src/components/Forms/IntakeForm.js
@@ -6,6 +6,7 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../Forms/IntakeForm.css';
 
+<<<<<<< HEAD
 // Counter used to determine if all form input boxes are valid.
 let trueCounter = 0;
 
@@ -131,10 +132,39 @@ export default function IntakeForm() {
 
     return(
         <Form onSubmit={intakeFormSubmission}>
+=======
+const initialValues = {
+    FirstName: '',
+    LastName: '',
+    EmailAddress: '',
+    PhoneNumber: '',
+    BestContactMethod: '',
+    ClientAddress: '',
+    ClientCity: '',
+    ClientZipCode: '',
+    Country: 'US',
+    DateOfBirth: '',
+    Gender: '',
+    Occupation: '',
+    ReasonForContact: '',
+    PoliceReportFiled: '',
+    Children: '',
+    AssitanceType: ''
+};
+
+
+export default function IntakeForm() {
+
+    const [values, setValues] = useState(initialValues);
+
+    return(
+        <Form>
+>>>>>>> Added Intake Form framework. Still needs validation and submission code to the repository.
             <div className="intake-form-style">
                 <div>
                     <h1 className="intake-title">Intake Form</h1>
                 </div>
+<<<<<<< HEAD
                 <br className="line-break" />
             
                 <Form.Group>
@@ -417,12 +447,179 @@ export default function IntakeForm() {
                         </Col>                            
                     </Row>  
                 </Form.Group> 
+=======
+            <br className="line-break" />
+            <Row>
+                <Col>
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control />
+                </Col>
+                <Col>
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control />
+                </Col>
+            </Row> 
+            <Row>
+                <Col>
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control />
+                </Col>                
+            </Row>  
+            <Row>
+                <Col lg="6">
+                    <Form.Label>Phone</Form.Label>
+                    <Form.Control />
+                </Col>                            
+            </Row>
+            <Row>
+                <Col>
+                    <Form.Label>What is the Best Contact Method?</Form.Label>
+                    <Form.Check 
+                        value="Phone call"
+                        type="radio"
+                        aria-label="radio 1"
+                        label="Phone Call"                        
+                    />
+                    <Form.Check 
+                        value="Text Message"
+                        type="radio"
+                        aria-label="radio 2"
+                        label="Text Messages"                        
+                    />
+                    <Form.Check 
+                        value="Email"
+                        type="radio"
+                        aria-label="radio 3"
+                        label="Email"                        
+                    />
+                </Col>                            
+            </Row> 
+            <Row>
+                <h3>Address</h3>
+                <Col>                    
+                    <Form.Control placeholder="Address 1"/>
+                    <Form.Label>Address 1</Form.Label>
+                </Col>                
+            </Row>  
+            <Row>
+                <Col>                    
+                    <Form.Control placeholder="Address 2"/>
+                    <Form.Label>Address 2</Form.Label>
+                </Col>                
+            </Row>  
+            <Row>
+                <Col lg="8">                    
+                    <Form.Control placeholder="Address 2"/>
+                    <Form.Label>City</Form.Label>
+                </Col> 
+                <Col lg="4">                    
+                    <Form.Control placeholder="Address 2"/>
+                    <Form.Label>State/Province</Form.Label>
+                </Col>                
+            </Row> 
+            <Row>
+                <Col>                    
+                    <Form.Control placeholder="Country"/>
+                    <Form.Label>Country</Form.Label>
+                </Col>                
+            </Row> 
+            <Row>
+                <h3>Date of Birth</h3>  
+                <Col lg="2">                        
+                    <Form.Control placeholder=""/>
+                    <Form.Label>MM</Form.Label>
+                </Col>
+                <Col lg="2">
+                    <Form.Control placeholder=""/>
+                    <Form.Label>DD</Form.Label>
+                </Col>
+                <Col lg="3">
+                    <Form.Control placeholder=""/>
+                    <Form.Label>YYYY</Form.Label>
+                 </Col>                    
+            </Row>  
+            <Row>
+                <Col>   
+                    <Form.Label>Gender</Form.Label>                 
+                    <Form.Control placeholder=""/>                    
+                </Col>                
+            </Row>   
+            <Row>
+                <Col>   
+                    <Form.Label>Occupation</Form.Label>                 
+                    <Form.Control placeholder=""/>                    
+                </Col>                
+            </Row>  
+            <Row>
+                <Col>   
+                    <h3>Questions</h3>           
+                </Col>                
+            </Row>   
+            <Row>
+                <Col>
+                    <Form.Label>What is the reason client is contacting us?</Form.Label>
+                    <Form.Check 
+                        value="Contemplating Suicide"
+                        type="radio"
+                        aria-label="radio 1"
+                        label="Contemplating Suicide"                        
+                    />
+                    <Form.Check 
+                        value="Domestic Harm / Abuse"
+                        type="radio"
+                        aria-label="radio 2"
+                        label="Domestic Harm / Abuse"                        
+                    />
+                    <Form.Check 
+                        value="Other"
+                        type="radio"
+                        aria-label="radio 3"
+                        label="Other"                        
+                    />
+                </Col>                            
+            </Row>      
+            <Row>
+                <Col>
+                    <Form.Label>Has a Police Report Been Filed?</Form.Label>
+                    <Form.Check 
+                        value="Yes"
+                        type="radio"
+                        aria-label="radio 1"
+                        label="Yes"                        
+                    />
+                    <Form.Check 
+                        value="Yes"
+                        type="radio"
+                        aria-label="radio 2"
+                        label="No"                        
+                    />                    
+                </Col>                            
+            </Row> 
+            <Row>
+                <Col>
+                    <Form.Label>Children?</Form.Label>
+                    <Form.Check 
+                        value="Yes"
+                        type="radio"
+                        aria-label="radio 1"
+                        label="Yes"                        
+                    />
+                    <Form.Check 
+                        value="No"
+                        type="radio"
+                        aria-label="radio 2"
+                        label="No"                        
+                    />                    
+                </Col>                            
+            </Row>  
+>>>>>>> Added Intake Form framework. Still needs validation and submission code to the repository.
             </div> 
             <Row className="submit-button">
                 <Button variant="primary" type="submit">Submit Form</Button> 
             </Row>                                            
         </Form>
     );   
+<<<<<<< HEAD
 }
 ////////////////////////////////////////////////////////////////////
 // VALILDATION FUNCTIONS FOR FORM                                //
@@ -474,4 +671,6 @@ function checkYear(year) {
             trueCounter += 1
         }
     }
+=======
+>>>>>>> Added Intake Form framework. Still needs validation and submission code to the repository.
 }
